d01dc9e14ce9660271942d4612f27df7673396c2
Emily Schomer <minischomer@gmail.com>
2014-04-08 15:38:44 -0500

    working jquery
    click & double-click features work now.
    additional instructions added.

--- js/app.js
+++ js/app.js
@@ -4,16 +4,25 @@ $(document).ready(function() {
 	$('#items').on('keydown', function(e) {
 		var item = $('input[id=items]').val();
 		if (e.keyCode == 13) {
-			$('.list').find('ul').append('<li>' +item+ '</li>');
+			$('.list').append('<div class="new">' +item+ '</div>');
 			$(this).val('');
 		}
 	});
 
 	$('#add').on('click', function() {
 		var item = $('input[id=items]').val();
-		$('.list').find('ul').append('<li>' +item+ '</li>');
-		$(this).val('');
-	})
+		$('.list').append('<div>' +item+ '</div>');
+		$('#items').val('');
+		$('#items').focus();
+	});
+
+	$('.list').on('click', '.new', function() {
+		$(this).toggleClass('bought');
+	});
+
+	$('.list').on('dblclick', '.new', '.bought', function() {
+			$(this).remove();
+	});
 
 });
 
